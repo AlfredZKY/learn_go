654ec1c0e5979c0ccd6e96190268fdbbc3ad81d4
AlfredZKY <17630302151@163.com>
2020-01-05 23:05:11 +0800

    update mutex

--- sync/mutexrw/mutexRW_test.go
+++ sync/mutexrw/mutexRW_test.go
@@ -1,6 +1,7 @@
 package mutexrw
 
 import (
+	"fmt"
 	"log"
 	"sync"
 	"testing"
@@ -24,7 +25,7 @@ func (c *counter) add(increment uint) uint {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.num += increment
-	return c.num 
+	return c.num
 }
 
 func count(c *counter) {
@@ -47,19 +48,19 @@ func count(c *counter) {
 			sign <- struct{}{}
 		}()
 
-		for j := 1; j <= 20; j++ {
+		for j := 1; j <= 5; j++ {
 			time.Sleep(time.Millisecond * 300)
 			log.Printf("The number in counter:%d [%d-%d]", c.number(), 1, j)
 		}
 	}()
-	
-	go func () {
-		defer func () {
-			sign <- struct {}{}
+
+	go func() {
+		defer func() {
+			sign <- struct{}{}
 		}()
-		for k:=1;k<=20;k++{
-			time.Sleep(time.Millisecond *300)
-			log.Printf("The number is counter:%d [%d-%d]",c.number(), 2, k)
+		for k := 1; k <= 5; k++ {
+			time.Sleep(time.Millisecond * 300)
+			log.Printf("The number is counter:%d [%d-%d]", c.number(), 2, k)
 		}
 	}()
 
@@ -68,7 +69,7 @@ func count(c *counter) {
 	<-sign
 }
 
-func redundantUnlock(){
+func redundantUnlock() {
 	var rwMu sync.RWMutex
 
 	// 实例1 解锁未锁定的互斥锁会立即引发 panic。会引发panic
@@ -77,7 +78,7 @@ func redundantUnlock(){
 	// 实例2 原因同上
 	// rwMu.RUnlock()
 
-	//实例3 
+	//实例3
 	rwMu.RLock()
 	// rwMu.RUnlock() 重复解锁
 	rwMu.RUnlock()
@@ -92,4 +93,23 @@ func TestMuntexRW(t *testing.T) {
 	c := counter{}
 	count(&c)
 	redundantUnlock()
+}
+
+func TestRWLock(t *testing.T) {
+	var rwm sync.RWMutex
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			fmt.Printf("Try to lock for reading... [%d]\n", i)
+			rwm.RLock()
+			fmt.Printf("Locked for reading. [%d]\n", i)
+			time.Sleep(time.Second * 2)
+			fmt.Printf("Try to unlock for reading... [%d]\n", i)
+			rwm.RUnlock()
+			fmt.Printf("Unlocked for reading. [%d]\n", i)
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Try to lock from writing...")
+	rwm.Lock()
+	fmt.Printf("Locked for writing.")
 }
