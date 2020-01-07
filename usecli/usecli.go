9e58430e82fa51b3606e711ad3d78f575728db74
AlfredZKY <17630302151@163.com>
2020-01-07 14:01:05 +0800

    add dir for learn cli

---
+++ usecli/usecli.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+func clifirst() {
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Usage = "fight the loneliness!"
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("Hello %q\n", c.Args().Get(0))
+		return nil
+	}
+	app.Run(os.Args)
+}
+
+func clisecond() {
+	app := cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Boom! I say!")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cliflag() {
+	app := cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "lang",
+				Value: "english",
+				Usage: "language for the freeting",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			name := "Nefertiti"
+			if c.NArg() > 0 {
+				name = c.Args().Get(0)
+			}
+			if c.String("lang") == "spanish" {
+				fmt.Println("hola", name)
+			} else {
+				fmt.Println("hello", name)
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cliflagdes(){
+	var language string
+	app := cli.App{
+		Flags :[]cli.Flag{
+			&cli.StringFlag{
+				Name:"lang",
+				Value:"english",
+				Aliases: []string{"l"},
+				Usage:"language for the freeting",
+				Destination:&language,
+			},
+		},
+		Action:func(c*cli.Context)error{
+			name := "someone"
+			if c.NArg()>0{
+				name = c.Args().Get(0)
+			}
+			fmt.Println(c.String("lang"))
+			if language == "spanish"{
+				fmt.Println("hola",name)
+			}else{
+				fmt.Println("Hello",name)
+			}
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+
+func main() {
+	// cli.NewApp().Run(os.Args)
+	//clifirst()
+	// clisecond()
+	// cliflag()
+	cliflagdes()
+}
