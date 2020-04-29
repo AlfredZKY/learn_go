c8361ab1335b9f261355bb769527fd89228779ec
AlfredZKY <17630302151@163.com>
2020-04-29 10:45:38 +0800

    add a file for learn slow quick pointer

---
+++ algorithm/dobulePointer.go
@@ -0,0 +1,22 @@
+package algorithm
+
+
+
+// DobulePointer 双指针
+func DobulePointer(nums []int) int {
+	n := len(nums)
+	if n < 2 {
+		return n
+	}
+
+	slow,quick := 0,1
+	for quick < n {
+		if nums[slow] < nums[quick] {
+			slow++
+			nums[slow]= nums[quick]
+		}
+		quick++
+	}
+	
+	return slow+1
+}
---
+++ algorithm/dobulePointer_test.go
@@ -0,0 +1,13 @@
+package algorithm
+
+import "testing"
+
+
+func TestDobulePointer(t *testing.T){
+	nums := []int{-1,0,1,2,-1,-4}
+	t.Log(nums)
+	QuickSort(nums)
+	t.Log(nums)
+	res := DobulePointer(nums)
+	t.Log(res)
+}
