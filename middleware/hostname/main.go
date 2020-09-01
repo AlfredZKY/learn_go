e388d8f3ea59129bfdcfd958caad90046eb4866f
quxiaobai@ipfs.black <qh@11.com>
2020-09-01 18:34:31 +0800

    update some files

--- middleware/hostname/main.go
+++ middleware/hostname/main.go
@@ -24,8 +24,8 @@ var (
 
 	SchedLogPath  = "/opt/ns"
 	taskTypesBack = map[string]taskPair{"ap": apTaskBak, "pc1": pc1TaskBak, "pc2": pc2TaskBak, "c1": c1TaskBak, "c2": c2TaskBak}
-	
-	taskTypes     = map[string]taskPair{"ap": apTask, "pc1": pc1Task, "pc2": pc2Task, "c1": c1Task, "c2": c2Task}
+
+	taskTypes    = map[string]taskPair{"ap": apTask, "pc1": pc1Task, "pc2": pc2Task, "c1": c1Task, "c2": c2Task}
 	scheduleTask = map[string]taskPair{"seal/v0/addpiece": apTask, "seal/v0/precommit/1": pc1Task, "seal/v0/precommit/2": pc2Task, "seal/v0/commit/1": c1Task, "seal/v0/commit/2": c2Task}
 )
 
@@ -36,7 +36,7 @@ type taskPair struct {
 // LOTUS_MINER_PATH config path
 const LOTUS_MINER_PATH = "/home/qh/zhou_project/"
 
-func init(){
+func init() {
 	initConfig()
 }
 
@@ -47,11 +47,12 @@ func initConfig() {
 	if err := v.ReadInConfig(); err != nil { // 搜索路径，并读取配置数据
 		// TODO 读取配置文件错误
 		logger.DebugWithFilePath(SchedLogPath+"/read_conifg_err.log", "Fatal error config file: %s \n", err)
-	}else{
+	} else {
 		for taskName, taskType := range taskTypesBack {
 			parseConfigToml(taskName, v, &taskType)
 		}
 	}
+
 }
 
 func parseConfigToml(key string, v *viper.Viper, taskType *taskPair) {
@@ -98,19 +99,19 @@ func removeWorkerFromTaskWorkerMap(hostname string) {
 func addWorkerFromTaskWorkerMap(hostname string) {
 	logger.DebugWithFilePath(SchedLogPath+"/new_worker.log", "trying to add new worker: %v\n", hostname)
 	var exist = false
-	for taskname, taskPairs := range taskTypesBack {
-		for hostnameexist, value := range taskPairs.hostnameValuePair {
+	for _, taskPairs := range taskTypesBack {
+		for hostnameexist := range taskPairs.hostnameValuePair {
 			if hostnameexist == hostname {
 				exist = true
 			}
+		}
 	}
 	if !exist {
 		logger.DebugWithFilePath(SchedLogPath+"/new_worker.log", "%v is not in current record, re-initializing record from config file...\n", hostname)
 		initConfig()
 	}
 
-	logger.DebugWithFilePath(SchedLogPath+"/new_worker.log", "trying to add new worker to running map: %v\n", hostname)
-
+	logger.DebugWithFilePath(SchedLogPath+"/new_worker.log", "trying to add new worker to map: %v\n", hostname)
 
 	for taskname, taskPairs := range taskTypesBack {
 		for hostnameexist, value := range taskPairs.hostnameValuePair {
@@ -120,10 +121,8 @@ func addWorkerFromTaskWorkerMap(hostname string) {
 			}
 		}
 	}
-
 }
 
-// CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build main.go
 func main() {
 	addWorkerFromTaskWorkerMap("idc21")
 	fmt.Println("======================================")
@@ -132,7 +131,7 @@ func main() {
 			fmt.Println(hostname, " ", value)
 		}
 	}
-	removeWorkerFromTaskWorkerMap("idc21")
+	removeWorkerFromTaskWorkerMap("idc23")
 	fmt.Println("======================================")
 	for _, taskPairs := range taskTypes {
 		for hostname, value := range taskPairs.hostnameValuePair {
